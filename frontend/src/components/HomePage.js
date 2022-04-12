0102d061f5f9d5be7ca6b26c59673fa13dc5309c
donnie <dyu@bizflow.com>
2022-04-12 21:48:39 +0900

    Homepage styling and API that checks the room code and redirect the page if the room code already exists.

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,18 +1,57 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom";
 import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
 import Room from "./Room";
+import { Grid, Button, ButtonGroup, Typography } from "@material-ui/core";
 
 export default class HomePage extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            roomCode: null,
+        };
+    }
+
+    async componentDidMount() {
+        fetch('/api/user-in-room')
+            .then((response) => response.json())
+            .then((data) => {
+                this.setState({
+                    roomCode: data.code
+                });
+            });
+    }
+
+    renderHomePage() {
+        return (
+            <Grid container spacing={3}>
+                <Grid item xs={12} align="center">
+                    <Typography variant="h3" component="h3">
+                        House Party
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <ButtonGroup disableElevation variant="contained" color="primary">
+                        <Button color="primary" to="/join" component={Link}>
+                            Join a Room
+                        </Button>
+                        <Button color="secondary" to="/create" component={Link}>
+                            Create a Room
+                        </Button>
+                    </ButtonGroup>
+                </Grid>
+            </Grid>
+        );
     }
 
     render() {
         return <Router>
             <Switch>
-                <Route exact path="/">This is home page.</Route>
+                <Route exact path="/" render={() => {
+                    return this.state.roomCode ? (<Redirect to={`/room/${this.state.roomCode}`}/>) : this.renderHomePage()
+                }}>
+                </Route>
                 <Route path="/join" component={RoomJoinPage}></Route>
                 <Route path="/create" component={CreateRoomPage}></Route>
                 <Route path="/room/:roomCode" component={Room}></Route>
