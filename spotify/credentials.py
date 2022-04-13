68a360778f98d75f82dfa064a8589d8d008c5b6e
donnie <dyu@bizflow.com>
2022-04-13 23:25:01 +0900

    Get authentication from Spotify and save auth info to the database.

---
+++ spotify/credentials.py
@@ -0,0 +1,5 @@
+# To-do : Try make this as evironmental variable.
+
+CLIENT_ID = "3400495d7db74b6cb281f4034f6b677e"
+CLIENT_SECRET = "2348df3c66f04f4f81087934150e1a1c"
+REDIRECT_URI = "http://127.0.0.1:7000/spotify/redirect"
